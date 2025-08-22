7e6c675b9d0e7bc0248b6b6a764fed100c0be71f
Andy Harris <andy@aharrisbooks.net>
2025-08-22 18:39:05 +0000

    added Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello {
+	public Hello(){
+		System.out.println("Hello, World!");
+	} // end Hello
+	
+	public static void main(String[] args){
+		new Hello();
+	} // end main
+} // end class def
